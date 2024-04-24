da296e3505138e8338896fe2a745935688f7b6fb
kennethjason07 <kenj7214@gmail.com>
2024-04-24 23:27:22 +0530

    23:27

--- src/app/AI-model/page.tsx
+++ src/app/AI-model/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-// import Plotly from 'plotly.js';
+import Plotly from 'plotly.js';
 
 const CrimeMap: React.FC = () => {
     const [crimeData, setCrimeData] = useState<any[]>([]);
